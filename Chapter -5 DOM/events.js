01171a161d2e1d252c5fdbe0a0f59de9a62747cf
Sharath <shararth.chinthas@gmail.com>
2024-04-23 22:29:11 -0500

    Added New Chapter

---
+++ Chapter -5 DOM/events.js
@@ -0,0 +1,21 @@
+
+// let clicked = document.querySelectorAll("li");
+// // console.log(clicked);
+
+// // clicked.addEventListener("click",(e)=>{
+// //   console.log("clicked button");
+// // })
+
+// // // clicked.forEach(function(e){
+// // //   e.addEventListener("click",function(e){
+// // //     console.log("item Clicked");
+// // //     console.log(e);
+// // //     // e.target();
+// // // })
+// // //   })
+
+let clicked = document.querySelector(".clickMe");
+// console.log(clicked);
+clicked.addEventListener("click",function(){
+  console.log("clicked");
+})
